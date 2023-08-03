36f9ca239ce6b85d6552e31e9c4e22422f91e630
AhasanRahman <shakilahasan98@gmail.com>
2023-08-03 19:43:30 +0600

    javascriptpractice

---
+++ firstjs.js
@@ -0,0 +1,58 @@
+let x = 250;
+let y =500;
+
+let z='x+y';
+
+console.log("x+y");
+
+
+const price1= 45;
+const price2= 78;
+let totall= price1+price2;
+
+
+console.log(totall);
+
+let p = 5;
+let q= 2;
+let r = p * q;
+
+let w = 3;
+let h = (100 + 50) * w;
+
+const gscs =['ahasan','fahad','anik', 'ridoy']
+
+var a=20;
+var b=30;
+var c=40;
+
+if (a<b){
+console.log("true");
+
+}
+else if (b>c){
+   console.log("true");
+}
+else {
+console.log("false") }
+
+
+
+function  abfunction(a, b) {
+    return (a/b)
+}
+
+console.log(abfunction(4, 2))
+
+function myFunction() {
+    alert("Hello World!");
+}
+var myinfo = {name:"ahasan", address:"uttara", contract:6348365975908};
+
+console.log(myinfo);
+
+for(var i=0; i<=20; i++){
+    if(i==5){
+        console.log("breaknow")
+    }
+}
